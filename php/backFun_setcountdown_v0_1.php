3b04c3fa533ea7f292937ed9c4a9df09f9aed778
Steven Andrei Barrios <32772384+steven1034@users.noreply.github.com>
2021-04-10 11:02:57 +0800

    Add files via upload

---
+++ php/backFun_setcountdown_v0_1.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include('../html/front_electionConfig_v0_2.php');
+
+$db = mysqli_connect('localhost', 'root', '', 'adminman');
+
+function function_alert($msg) { 
+      
+    // Display the alert box  
+    echo "<script>alert('$msg');</script>"; 
+} 
+
+//set timeframe
+   if (isset($_POST['savesched'])) {
+
+      $date=$_POST['date'];
+      $dateEnd=$_POST['dateEnd'];
+      $tstart= $_POST['tstart'];
+      $tends= $_POST['tends'];
+
+      
+            //updating the table
+      $vtevent = "INSERT INTO vote_event (`start_date`,`end_date`) 
+      VALUES('$date $tstart', '$dateEnd $tends')";
+    mysqli_query($db, $vtevent);
+    function_alert("saved"); 
+
+   }else{
+      function_alert("failed"); 
+   }
+
+
+   //edit timeframe
+if (isset($_POST['editsched'])) {
+
+    $date=$_POST['date'];
+    $dateEnd=$_POST['dateEnd'];
+    $tstart= $_POST['tstart'];
+    $tends= $_POST['tends'];
+
+    
+    //updating the table
+    $edit =  "UPDATE `vote_event` SET start_date='$date $tstart', end_date='$dateEnd $tends ' WHERE vote_event_id = 1";
+  mysqli_query($db, $edit);
+  function_alert("saved"); 
+
+ }else{
+    function_alert("failed"); 
+ }
+
+
+
+   ?>
