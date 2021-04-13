9cc3992b3d69d35703653c6339afccb7e0beab6a
Steven Andrei Barrios <32772384+steven1034@users.noreply.github.com>
2021-04-13 16:01:59 +0800

    Update backFun_setcountdown_v0_1.php

--- php/backFun_setcountdown_v0_1.php
+++ php/backFun_setcountdown_v0_1.php
@@ -10,37 +10,10 @@ function function_alert($msg) {
     echo "<script>alert('$msg');</script>"; 
 } 
 
-$count = 0;
-$res = mysqli_query($db, "SELECT * FROM vote_event");
-while ($row = mysqli_fetch_array($res)) { 
-$count += 1;
-}
-
-//set timeframe
-if($count == 1){
-   if (isset($_POST['savesched'])) {
-
-      $date=$_POST['date'];
-      $dateEnd=$_POST['dateEnd'];
-      $tstart= $_POST['tstart'];
-      $tends= $_POST['tends'];
-
-  
-            //SET TIMEFRAME
-      $vtevent = "INSERT INTO vote_event (`start_date`,`end_date`) 
-      VALUES('$date $tstart', '$dateEnd $tends')";
-      mysqli_query($db, $vtevent);
-      function_alert("saved");
-      
-       }
-   }else{
-      function_alert("Timeframe is already set");
-   }
-      
-
-
    //edit timeframe
 if (isset($_POST['editsched'])) {
+      
+     $truncate = mysqli_query($db, "TRUNCATE TABLE vote_event"); 
 
     $date=$_POST['date'];
     $dateEnd=$_POST['dateEnd'];
@@ -49,8 +22,9 @@ if (isset($_POST['editsched'])) {
 
     
     //updating the table
-    $edit =  "UPDATE `vote_event` SET start_date='$date $tstart', end_date='$dateEnd $tends ' WHERE vote_event_id = 1";
-  mysqli_query($db, $edit);
+     $vtevent = "INSERT INTO vote_event (`start_date`,`end_date`) 
+      VALUES('$date $tstart', '$dateEnd $tends')";
+      mysqli_query($db, $vtevent);
   function_alert("updating successful"); 
 
  }
