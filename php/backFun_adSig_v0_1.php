3cd8672afcd4c35379f2ff8f32f1b66dcf93985f
vertex009 <79442937+vertex009@users.noreply.github.com>
2021-04-20 00:10:30 +0800

    Update backFun_adSig_v0_1.php

--- php/backFun_adSig_v0_1.php
+++ php/backFun_adSig_v0_1.php
@@ -8,6 +8,16 @@ session_start();
 
 $signame = $_POST['signame'];
 $sigpos = $_POST['sigpos'];
+
+$dup_check= "SELECT * FROM signatory_table WHERE signame='$signame' LIMIT 1";
+$dup_query = mysqli_query($connection, $dup_check);
+$name = mysqli_fetch_assoc($dup_query);
+
+if ($name['signame'] === $signame) {
+    array_push($errors, "Signatory already exists");
+    echo "Signatory already exists";
+}
+else{
 $insert = "INSERT INTO signatory_table (`signame`, `sigpos`) VALUES ('$signame', '$sigpos')";
 $query = mysqli_query($connection, $insert);
 
@@ -16,5 +26,5 @@ $_SESSION['action'] = 'added Signatory : ' . $_POST['signame'];
 include 'backFun_adLogs_v0_1.php';
 
 echo "Signatory Saved Successfully";
-
+}
 ?>
