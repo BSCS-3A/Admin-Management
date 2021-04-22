3de15ea4ac23e433032fb4d2a3dde3c1a2e459b5
Kerby Maravilla Bartolay <70627486+kerbae@users.noreply.github.com>
2021-04-22 19:10:40 +0800

    Update backFun_actLogs_v0_1.php

--- php/backFun_actLogs_v0_1.php
+++ php/backFun_actLogs_v0_1.php
@@ -1,21 +1,24 @@
 <?php
+	session_start();
+
 	//Note set actions for every functionality i.e from every functionality file add and update the code below this line
 	//$_SESSION['action'] = 'Added an Account' or 'Updated an Account' or 'Set and Sent Reminder' or 'Sent last Reminder' or 'Set Signatories' or 'Set Election Time and Date'
 
 	// Create connection
-	$connection = mysqli_connect("localhost","root","");
-	$db = mysqli_select_db($connection, 'buceils_db');
+	include "db_conn.php";
 
 	$activity_description = $_SESSION['action'];
 	unset($_SESSION['action']);
 
-	$admin_id = $_SESSION['']; // removes backslashes
-	$admin_id = mysqli_real_escape_string($connection, $admin_id); //escapes special characters in a string
-
-	$query = "INSERT INTO table_name (admin_id, activity_description, activity_date, activity_time)
-			values ('$admin_id', '$activity_description', '$activity_date', 'activity_time')";
-	
+	$admin_id = $_SESSION['admin_id'];
 	unset($_SESSION['admin_id']);
 
-	mysqli_query($connection, $query);
+	date_default_timezone_set('Asia/Manila');
+	$activity_date = date('Y-m-d');
+	$activity_time = date('H:i:s');
+
+	$query = "INSERT INTO admin_activity_log (admin_id, activity_description, activity_date, activity_time)
+			  values ('$admin_id', '$activity_description', '$activity_date', '$activity_time')";
+
+	mysqli_query($conn, $query);
 ?>
