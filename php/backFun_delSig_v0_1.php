ed45b875bbb3f6c87641511cc99c98681148f921
vertex009 <79442937+vertex009@users.noreply.github.com>
2021-04-28 21:58:06 +0800

    Update backFun_delSig_v0_1.php

--- php/backFun_delSig_v0_1.php
+++ php/backFun_delSig_v0_1.php
@@ -2,21 +2,45 @@
 $connection = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($connection, 'buceils_db');
 
-session_start();
+$sigfname = (isset($_POST['signatory_fname']) ? $_POST['signatory_fname'] : '');
+$siglname = (isset($_POST['signatory_lname']) ? $_POST['signatory_lname'] : '');
+$sigpos = (isset($_POST['signatory_position']) ? $_POST['signatory_position'] : '');
+$file = file_get_contents('../php/sig_array.txt');
+$decode = json_decode($file, true);
 
+$fname_query = "SELECT * FROM admin_table WHERE admin_fname = '$sigfname'";
+$lname_query = "SELECT * FROM admin_table WHERE admin_lname = '$siglname'";
+$position_query = "SELECT * FROM admin_table WHERE comelec_position = '$sigpos'";
 
- $id = (isset($_POST['Delete_ID']) ? $_POST['Delete_ID'] : '');
-$delete = "DELETE FROM signatory_table WHERE id ='$id'";
-$query = mysqli_query($connection, $delete);
+$fname_query_run = mysqli_query($connection, $fname_query);
+$lname_query_run = mysqli_query($connection, $lname_query);
+$position_query_run = mysqli_query($connection, $position_query);
 
-if($query){
-echo "Signatory Deleted Successfully";
- 
-   //For Logs
-   $signame = "SELECT signame FROM signatory_table WHERE id = '$_POST['DELETE_ID']' ";
-   $_SESSION['action'] = 'deleted Signatory : ' . $signame ;
-   include 'backFun_adLogs_v0_1.php';
+if(mysqli_num_rows($fname_query_run) >0){
+  if(mysqli_num_rows($lname_query_run) >0){
+    if(mysqli_num_rows($position_query_run) >0){
+
+      $sql = "SELECT * FROM admin_table WHERE admin_lname ='$siglname'";
+      $result = mysqli_query($connection,$sql);
+      if(mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+        $source = $row['admin_id'];
+        $id = explode(",",$decode);
+        $id = array_filter($id);
+        $key = array_search($source, $id);
+        unset($id[$key]);
+        $string = implode (", ", $id);
+        $encodedString = json_encode($string);
+        file_put_contents('sig_array.txt',($encodedString));
+        echo"<script language='javascript'>
+        alert('Signatory Deleted');
+        </script>
+        ";
+
+        }
+      }
+    }
+  }
 }
-else
-echo "Failed to Delete Signatory";
+
 ?>
